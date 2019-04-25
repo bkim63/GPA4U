92c01ba5c1726d1784d459395b438485f5c88726
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-25 11:32:14 -0400

    created kim my assessments activity fixed a bug where min sdk version cannot be lower than 14.

---
+++ app/src/main/java/com/bumjinkim/GPA4U/KimMyAssessmentsActivity.java
@@ -0,0 +1,14 @@
+package com.bumjinkim.GPA4U;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class KimMyAssessmentsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kim_my_assessments);
+    }
+
+}
