e558b7b0630a098a24ada3f151574bbd2eb8e61b
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-25 19:06:15 -0400

    my assessment activity updated

--- app/src/main/java/com/bumjinkim/GPA4U/KimMyAssessmentsActivity.java
+++ app/src/main/java/com/bumjinkim/GPA4U/KimMyAssessmentsActivity.java
@@ -1,14 +1,33 @@
 package com.bumjinkim.GPA4U;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class KimMyAssessmentsActivity extends Activity {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private KimMyAssessmentAdapter kimMyAssessmentAdapter;
+
+    private String[] names = new String[]{"UIMA", "Intro to Psychology", "Abnormal Psychology"};
+    private String[] grades = new String[]{"A", "A", "A"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kim_my_assessments);
-    }
 
+        setContentView(R.layout.activity_kim_my_assessments);
+
+        recyclerView = findViewById(R.id.kim_my_assessment_list_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        kimMyAssessmentAdapter = new KimMyAssessmentAdapter(names, grades);
+        recyclerView.setAdapter(kimMyAssessmentAdapter);
+
+    }
 }
