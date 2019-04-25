5f5610b37fee81a1eba24e9c5f28315f6bd72a85
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-25 11:35:40 -0400

    Created initial add courses activity Java version to 1.8

---
+++ app/src/main/java/com/bumjinkim/GPA4U/KimAddCourseActivity.java
@@ -0,0 +1,14 @@
+package com.bumjinkim.GPA4U;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class KimAddCourseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kim_add_course);
+    }
+
+}
