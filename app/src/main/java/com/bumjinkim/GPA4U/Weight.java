9da589783c83dae6e933fd30757c4a9991e784d3
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-26 19:25:40 -0400

    database weight model

---
+++ app/src/main/java/com/bumjinkim/GPA4U/Weight.java
@@ -0,0 +1,13 @@
+package com.bumjinkim.GPA4U;
+
+public class Weight {
+    public String name;
+    public Double percent;
+    public String course;
+
+    public Weight(String name, Double percent, String course) {
+        this.name = name;
+        this.percent = percent;
+        this.course = course;
+    }
+}
