91e927211b85fb5f040100c90207c31d39eb825f
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-25 23:20:55 -0400

    Back button and bottom navigation for Add Course screen

--- app/src/main/java/com/bumjinkim/GPA4U/KimAddAssessmentActivity.java
+++ app/src/main/java/com/bumjinkim/GPA4U/KimAddAssessmentActivity.java
@@ -2,13 +2,48 @@ package com.bumjinkim.GPA4U;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class KimAddAssessmentActivity extends Activity {
+public class KimAddAssessmentActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_my_courses:
+                    finish();
+                    return true;
+                case R.id.navigation_my_gpa:
+
+                    return true;
+            }
+            return false;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kim_add_assessment);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
 }
--- app/src/main/java/com/bumjinkim/GPA4U/KimMyCoursesFragment.java
+++ app/src/main/java/com/bumjinkim/GPA4U/KimMyCoursesFragment.java
@@ -1,8 +1,11 @@
 package com.bumjinkim.GPA4U;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -23,6 +26,15 @@ public class KimMyCoursesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View fragmentKimMyCourses = inflater.inflate(R.layout.fragment_kim_my_courses, container, false);
+
+        FloatingActionButton fab = fragmentKimMyCourses.findViewById(R.id.kim_add_course_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getActivity(), KimAddCourseActivity.class);
+                getActivity().startActivity(myIntent);
+            }
+        });
 
         recyclerView = fragmentKimMyCourses.findViewById(R.id.kim_my_course_list_view);
         recyclerView.setHasFixedSize(true);
