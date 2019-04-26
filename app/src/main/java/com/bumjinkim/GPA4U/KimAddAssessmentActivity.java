33db706b43ec202ed439bf14b4fa0f4788fcab60
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-26 12:57:04 -0400

    dropdown values added

--- app/src/main/java/com/bumjinkim/GPA4U/KimAddAssessmentActivity.java
+++ app/src/main/java/com/bumjinkim/GPA4U/KimAddAssessmentActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class KimAddAssessmentActivity extends AppCompatActivity {
 
@@ -39,6 +41,17 @@ public class KimAddAssessmentActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        String[] spinnerItems = new String[] {
+                "Assignments", "Project", "Participation", "Quiz"
+        };
+        Spinner s = (Spinner) findViewById(R.id.kim_add_assessment_type);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, spinnerItems);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        s.setAdapter(adapter);
     }
 
     @Override
