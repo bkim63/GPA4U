4df8857164f47558cc45555b283d7b8c6e775611
Bumjin Kim (bkim63) <bkim63@jhu.edu>
2019-04-25 23:20:19 -0400

    Textviews for Add course screen

--- app/src/main/java/com/bumjinkim/GPA4U/KimAddCourseActivity.java
+++ app/src/main/java/com/bumjinkim/GPA4U/KimAddCourseActivity.java
@@ -2,13 +2,47 @@ package com.bumjinkim.GPA4U;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class KimAddCourseActivity extends Activity {
+public class KimAddCourseActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_my_courses:
+                    finish();
+                    return true;
+                case R.id.navigation_my_gpa:
+
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kim_add_course);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public boolean onSupportNavigateUp(){
+        finish();
+        return true;
+    }
 }
